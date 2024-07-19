cabe44f4346965a089fce2bcc501896571890f34
RalpharUnderscore <98220016+RalpharUnderscore@users.noreply.github.com>
2024-07-19 20:56:20 +0700

    manual and init game

---
+++ game.py
@@ -0,0 +1,60 @@
+if __name__ == "__main__":
+    input("Please run main.py to play the game\n")
+    quit()
+
+import random
+
+# Decklist 
+decklist: list = [] # Full Deck
+deck: list = [] # Currently what's in the deck
+
+duplicate: int
+variance: int = 2
+
+# Hand
+your_hand: list = []
+opps_hand: list = []
+
+
+# Bound
+your_bound: int = 24
+opps_bound: int = 24
+
+
+
+def init_game() -> None:
+    global decklist
+    global duplicate
+    global your_bound
+    global opps_bound
+
+
+    # Setup Decklist
+    decklist = [x for x in range(2, 13)]
+    duplicate = random.choice(decklist)
+    decklist.append(duplicate)
+    decklist.sort()
+
+    # Set Bound
+    your_bound = random.randint(16, 34)
+    opps_bound = your_bound + random.randint(-variance, variance)
+
+    # Gameloop, if your_turn returns false (game ended), break
+    while True:
+        if not your_turn(): break
+
+
+def your_turn() -> bool:
+    print(your_bound)
+    print_display()
+
+
+
+def print_display() -> None:
+    print("------------------------------------")
+    print("|                                   |")
+    print("|                                   |")
+    print("|                                   |")
+    print("------------------------------------")
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+import game as Game
+
+
+skill_id: int = 0
+
 def main() -> None:
+    display_title()
+
+    player_response = input(">").lower()
+    match player_response:
+        case "play":
+            Game.init_game()
+
+        case "skill":
+            change_skill()
+
+        case "help":
+            f = open("goforx.help", "r")
+            print(f.read())
+            f.close()
+        
+        case "quit":
+            quit()
+        
+        case _:
+            print(f"Unknown Command \'{player_response}\'")
+
+
+def display_title() -> None:
     print("-----------------------")
     print("|                     |")
     print("| Let's play GoForX!! |")
@@ -11,25 +39,13 @@ def main() -> None:
     print("\'skill\': Change your skill")
     print("\'help\': Displays the GoForX manual")
     print("\'quit\': Quit the Game")
-    
-    player_response = input().lower()
-    match player_response:
-        case "play":
-            pass
 
-        case "skill":
-            pass
 
-        case "help":
-            f = open("goforx.help", "r")
-            print(f.read())
-            f.close()
-        
-        case "quit":
-            quit()
-        
-        case _:
-            print(f"Unknown Command \'{player_response}\'")
+def change_skill() -> None:
+    id = int(input("Change skill to: "))
+    f = open("savefile.txt", "w")
+    print(f.write(str(id)))
+    f.close()
 
 
 if __name__ == "__main__":
