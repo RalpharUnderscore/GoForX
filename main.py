d9fc3b99d6773826570618ca222818fe10d2648f
RalpharUnderscore <98220016+RalpharUnderscore@users.noreply.github.com>
2024-08-03 21:33:31 +0700

    change skills

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import game as Game
 
 
 skill_id: int = 0
+sklist: dict = {
+    0: "RACK",
+    1: "RETURN",
+    2: "SPIN",
+    3: "CLONE",
+    4: "LUCKY DRAW",
+    5: "MINUS TWO",
+    6: "CLAMP",
+    7: "UP TWO",
+    8: "SAVANT",
+    9: "DESPERADO",
+}
+
+
 
 def main() -> None:
     display_title()
@@ -24,8 +38,6 @@ def main() -> None:
             print(f.read())
             f.close()
         
-
-
         case "quit":
             quit()
         
@@ -39,14 +51,25 @@ def display_title() -> None:
     print("| Let's play GoForX!! |")
     print("|                     |")
     print("-----------------------")
-    print("Current skill: Rack")
+    savefile = open("savefile.txt", "r")
+    id = savefile.read()
     
+    make_sklist_is_valid(savefile)
+    print(f"Current skill: {sklist[int(id)]}")
     print("Command List:")
     print("\'play\': Play a Round")
     print("\'skill\': Change your skill")
     print("\'help\': Displays the GoForX how-to-play")
     print("\'manual\': Displays the long-winded GoForX manual")
     print("\'quit\': Quit the Game")
+
+
+def make_sklist_is_valid(savefile):
+    text = savefile.read()
+    if text not in sklist:
+        savefile2 = open("savefile.txt", "w")
+        savefile2.write("0")
+        savefile2.close()
 
 
 def change_skill() -> None:
@@ -56,11 +79,19 @@ def change_skill() -> None:
 
 
     id = int(input("Change skill to (ID): "))
-    match id:
-        case 0:
-            pass
-    f = open("savefile.txt", "w")
-    print(f.write(str(id)))
+    
+    savefile = open("savefile.txt", "w")
+    savefile.write(str(id))
+    savefile.close()
+    print()
+    print(f"Skill equipped: {sklist[id]}")
+    print()
+    print("Type \'skill\' in game to use your skill.")
+    print("You will be prompted to guess the currnet game's duplicate card.")
+    print("Guess correctly and you can use your skill.")
+    print("You can only use your skill once per game")
+
+    print()
     f.close()
 
 
